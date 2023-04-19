ec6158a52495ee49c35c817aa322ded57cf37ac1
dotnet-code <@dotnet-code>
2023-04-19 16:19:02 +0530

    commit from code202304191902

---
+++ test_a_13/Controllers/LambdaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using test_a_13;
+
+public class LambdaController : ApiController
+{
+    // GET api/lambda
+    public IHttpActionResult Get()
+    {
+        var result = Enumerable.Range(1, 10)
+            .Select(x => Math.Pow(x, 2))
+            .ToList();
+
+        return Ok(result);
+    }
+
+    // POST api/lambda
+    public async Task<IHttpActionResult> Post([FromBody]int[] array)
+    {
+        if (array == null)
+            return BadRequest();
+
+        var result = await Task.Run(() => array
+            .Select(x => Math.Pow(x, 2))
+            .ToList());
+
+        return Ok(result);
+    }
+}
